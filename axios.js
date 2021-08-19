b2e86640d09ea319fba57729077a83d42b3f3429
mngduyphuong <you@example.com>
2021-08-19 12:11:13 +0700

    finish getData, connected to API

--- axios.js
+++ axios.js
@@ -1,7 +1,19 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+
+const getData = () => {
+    var ID = document.getElementById('idTest').value;
+    axios.get('https://reqres.in/api/users/' + ID)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+};
 
 const sendData = () => {};
 
