99b63e36baa9eb2226d7fdcc8450db5100ebfb1c
mngduyphuong <you@example.com>
2021-08-19 15:03:46 +0700

    finish get axios

--- axios.js
+++ axios.js
@@ -8,13 +8,25 @@ const getData = () => {
   .then(function (response) {
     // handle success
     console.log(response);
+    displayData(response.data);
   })
   .catch(function (error) {
     // handle error
     console.log(error);
+    document.getElementById("dataError").innerHTML = "Invalid ID";
+    document.getElementById("dataID").innerHTML = "";
+  document.getElementById("dataName").innerHTML = "";
+  document.getElementById("dataEmail").innerHTML = "";
+  document.getElementById("dataImg").src = "";
   });
 };
-
+function displayData(test){
+  document.getElementById("dataID").innerHTML = test.data.id;
+  document.getElementById("dataName").innerHTML = test.data.first_name + " " + test.data.last_name;
+  document.getElementById("dataEmail").innerHTML = test.data.email;
+  document.getElementById("dataImg").src = test.data.avatar;
+  document.getElementById("dataError").innerHTML = "";
+};
 const sendData = () => {};
 
 getBtn.addEventListener('click', getData);
